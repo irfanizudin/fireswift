15969715efa5e1fd181cf8a8aa0ae7721df18dbe
irfanizudin <irfanizzudyn@gmail.com>
2022-12-27 09:58:27 +0700

    Initial Commit

---
+++ firebase-swift/firebase_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_swiftApp.swift
+//  firebase-swift
+//
+//  Created by Irfan Izudin on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct firebase_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
